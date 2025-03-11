f2a90951046958de4813bc9485631d5413409f26
Benedikt Liebs <benedikt.liebs@fellowpro.com>
2025-03-11 14:38:55 +0000

    Deploy to Github

---
+++ assets/axios.1088cc14.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.0ecf3f3d.js";import{a}from"./axios.ab682c6b.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
